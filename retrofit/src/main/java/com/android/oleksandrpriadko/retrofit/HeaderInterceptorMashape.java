78bf04ccd2cb7b0a7ac08cf4e14fd0538313ed27
Oleksandr Priadko <oleksandrpriadko@gmail.com>
2018-05-25 13:11:43 +1000

    Resolves #13 HeaderInterceptor - Inner class Header instead of SimplePair

--- retrofit/src/main/java/com/android/oleksandrpriadko/retrofit/HeaderInterceptorMashape.java
+++ retrofit/src/main/java/com/android/oleksandrpriadko/retrofit/HeaderInterceptorMashape.java
@@ -5,15 +5,15 @@ import android.support.annotation.NonNull;
 public class HeaderInterceptorMashape extends HeaderInterceptor {
     @SuppressWarnings("SpellCheckingInspection")
     private static final String MASHAPE_TITLE
-            = "X-Mashape-Key";
+        = "X-Mashape-Key";
     @SuppressWarnings("SpellCheckingInspection")
     private static final String MASHAPE_VALUE
-            = "ZqZDokvSBlmshEMoSxzqhog0kOcOp1tnJgvjsnGOhZhcrGnywl";
+        = "ZqZDokvSBlmshEMoSxzqhog0kOcOp1tnJgvjsnGOhZhcrGnywl";
 
     @NonNull
     @Override
-    protected SimplePairStrings[] getHeaders() {
-        SimplePairStrings simplePairStrings = new SimplePairStrings(MASHAPE_TITLE, MASHAPE_VALUE);
-        return new SimplePairStrings[]{simplePairStrings};
+    protected Header[] getHeaders() {
+        Header header = new Header(MASHAPE_TITLE, MASHAPE_VALUE);
+        return new Header[]{header};
     }
 }
