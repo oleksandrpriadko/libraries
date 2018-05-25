9bd4924c8b4441b88eddbf2e273418d0ea7661a2
Oleksandr Priadko <oleksandrpriadko@gmail.com>
2018-05-25 12:38:41 +1000

    Resolves #6 DataCacheBuilder - Builder for DataCache

--- mvp/src/main/java/com/android/oleksandrpriadko/mvp/interactor/DataCache.java
+++ mvp/src/main/java/com/android/oleksandrpriadko/mvp/interactor/DataCache.java
@@ -13,10 +13,10 @@ public class DataCache<D> {
     private final Cache<String, D> mDataCache;
     private final String mKey;
 
-    public DataCache(String key,
-                     @IntRange(from = 1) int cacheSizeMb,
-                     @IntRange(from = 1L) long expTime,
-                     TimeUnit expTimeUnit) {
+    private DataCache(String key,
+                      @IntRange(from = 1L) long cacheSizeMb,
+                      @IntRange(from = 0L) long expTime,
+                      TimeUnit expTimeUnit) {
         this.mKey = key;
         this.mDataCache = CacheBuilder.newBuilder()
             .maximumSize(cacheSizeMb)
@@ -35,5 +35,33 @@ public class DataCache<D> {
 
     public final void clearCache() {
         this.mDataCache.invalidateAll();
+    }
+
+    public static class Builder {
+
+        private final String mKey;
+        private long mSizeMb;
+        private TimeUnit mExpirationTimeUnit;
+        private long mExpirationDuration;
+
+        public Builder(String key) {
+            mKey = key;
+        }
+
+        public Builder maxSizeMB(@IntRange(from = 1L) long sizeMb) {
+            mSizeMb = sizeMb;
+            return this;
+        }
+
+        public Builder expirationTime(TimeUnit expireTimeUnit,
+                                      @IntRange(from = 0L) long expirationDuration) {
+            mExpirationTimeUnit = expireTimeUnit;
+            mExpirationDuration = expirationDuration;
+            return this;
+        }
+
+        public <D> DataCache<D> build() {
+            return new DataCache<>(mKey, mSizeMb, mExpirationDuration, mExpirationTimeUnit);
+        }
     }
 }
